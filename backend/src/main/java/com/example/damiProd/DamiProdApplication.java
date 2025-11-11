6b7250a62af9dbc7f774740e6a48521773051eca
Andrei Dan <A.Dan@student.tudelft.nl>
2025-11-11 20:25:08 +0100

    init commit

---
+++ backend/src/main/java/com/example/damiProd/DamiProdApplication.java
@@ -0,0 +1,13 @@
+package com.example.damiProd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamiProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamiProdApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/damiProd/DamiProdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.damiProd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamiProdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
