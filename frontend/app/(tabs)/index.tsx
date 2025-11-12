79c11bc9539defaeda7836dd43df7eabdbcb41ff
Andrei Dan <A.Dan@student.tudelft.nl>
2025-11-12 17:58:39 +0100

    resolved conflict

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -6,11 +6,7 @@ import { Text, View } from '@/components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
       <Text style={styles.title}>Tab testLucaBuca</Text>
-=======
-      <Text style={styles.title}>Tab test</Text>
->>>>>>> 4d6e7ef0972725727d09f09f2ad28a7682cffcc2
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
