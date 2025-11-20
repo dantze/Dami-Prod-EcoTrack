af03454ce899bdbb00b9bba3dd13375cd2922ff4
BirisBogdan <biris.bogdan05@gmail.com>
2025-11-20 18:42:00 +0200

    Revert

--- backend/src/main/java/com/example/damiProd/domain/TaskStatus.java
+++
@@ -1,8 +0,0 @@
-package com.example.damiProd.domain;
-
-public enum TaskStatus {
-    NEW,            // Sarcina creată de dispecer, dar neîncepută
-    IN_PROGRESS,    // Șoferul a ajuns la locație și lucrează
-    COMPLETED,      // Sarcina finalizată cu succes
-    CANCELLED       // Sarcina anulată
-}
--- backend/src/main/java/com/example/damiProd/domain/TaskType.java
+++
@@ -1,8 +0,0 @@
-package com.example.damiProd.domain;
-
-public enum TaskType {
-    PLACEMENT,      // Plasare toaletă
-    PICKUP,         // Ridicare/Preluare
-    SANITIZATION,   // Igienizare/Vidanjare
-    MAINTENANCE     // Mentenanță/Reparații
-}
