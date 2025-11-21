cd8860d1836f7ab892669849aaec3e34885cb964
Andrei Dan <A.Dan@student.tudelft.nl>
2025-11-21 22:23:47 +0100

    reviewd client

--- backend/src/main/java/com/example/damiProd/domain/Client.java
+++ backend/src/main/java/com/example/damiProd/domain/Client.java
@@ -1,8 +1,15 @@
 package com.example.damiProd.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
