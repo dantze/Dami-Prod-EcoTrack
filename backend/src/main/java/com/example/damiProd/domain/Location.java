541c1339b585e497857634ea0a0bcf4ccc311e84
Andrei Dan <A.Dan@student.tudelft.nl>
2025-11-21 22:18:43 +0100

    Location entity

---
+++ backend/src/main/java/com/example/damiProd/domain/Location.java
@@ -0,0 +1,46 @@
+package com.example.damiProd.domain;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "locations")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    @Embedded
+    private Coordinates coordinates;
+
+    public Location() {
+    }
+
+    public Location(Long id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    @Embeddable
+    @Getter
+    @Setter
+    public static class Coordinates {
+        private double latitude;
+        private double longitude;
+    }
+}
