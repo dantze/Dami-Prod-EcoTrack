e8cbe8209de05bbd30d6a68f4a1ccb30cd92baec
Andrei Dan <A.Dan@student.tudelft.nl>
2025-11-21 22:28:09 +0100

    Added Client and Individuals as entities

---
+++ backend/src/main/java/com/example/damiProd/domain/Company.java
@@ -0,0 +1,39 @@
+package com.example.damiProd.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "companies")
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private String CUI;
+
+    private String adminName;
+
+    public Company() {
+    }
+
+    public Company(Long id, String name, String address, String CUI, String adminName) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.CUI = CUI;
+        this.adminName = adminName;
+    }
+
+}
