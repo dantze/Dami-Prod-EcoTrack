373cf7d8713fd0389543f612bc1838e60e9ed9b8
Andrei Dan <A.Dan@student.tudelft.nl>
2025-11-22 17:06:43 +0100

    apikey

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -24,7 +24,7 @@ export default {
             },
             config: {
                 googleMaps: {
-                    apiKey: process.env.GOOGLE_MAPS_API_KEY
+                    apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
                 }
             },
             edgeToEdgeEnabled: true,
